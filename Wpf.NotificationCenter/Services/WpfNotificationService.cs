776b7b6ee757a103701a93a2b2c25141db85df3e
Christopher Winland <chrislw77@gmail.com>
2023-10-20 14:34:07 -0400

    Add Dispatcher

--- Wpf.NotificationCenter/Services/WpfNotificationService.cs
+++ Wpf.NotificationCenter/Services/WpfNotificationService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Threading;
 using Wpf.NotificationCenter.Enums;
 using Wpf.NotificationCenter.Extensions;
 
@@ -12,23 +13,42 @@ namespace Wpf.NotificationCenter.Services
     /// <seealso cref="Wpf.NotificationCenter.Services.IWpfNotificationService" />
     public class WpfNotificationService : IWpfNotificationService
     {
-        private static NotificationCenter GetNotificationCenter(string? notificationCenterName = null) =>
-            Application.Current?.MainWindow?.FindChild<NotificationCenter>(notificationCenterName) ??
-            throw NotFound(notificationCenterName);
+        #region Fields
+
+        private readonly Dispatcher dispatcher;
+
+        #endregion
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="WpfNotificationService" /> class.
+        /// </summary>
+        /// <param name="dispatcher">The dispatcher.</param>
+        public WpfNotificationService(Dispatcher? dispatcher = null)
+        {
+            dispatcher ??= Application.Current?.Dispatcher ?? Dispatcher.CurrentDispatcher;
+
+            this.dispatcher = dispatcher ?? throw new ArgumentNullException(nameof(dispatcher));
+        }
+
+        private static NotificationCenter GetNotificationCenter(string? notificationCenterName = null) => Application.Current?.MainWindow?.FindChild<NotificationCenter>(notificationCenterName) ??
+                   throw NotFound(notificationCenterName);
 
         private static KeyNotFoundException NotFound(string? name = null) => new($"{name ?? "Notification Center"} not found.");
 
         #region IWpfNotificationService
 
         /// <inheritdoc />
-        public void Close(Notification.Notification notification, string? notificationCenterName = null) =>
-            GetNotificationCenter(notificationCenterName).RemoveNotification(notification);
-
+        public void Close(Notification.Notification notification, string? notificationCenterName = null) => GetNotificationCenter(notificationCenterName).RemoveNotification(notification);
 
         /// <inheritdoc />
         public Notification.Notification Create(string title, string text, NotificationType notificationType = NotificationType.Information,
             string? notificationCenterName = null, AlertType alertType = AlertType.All)
         {
+            if (!dispatcher.CheckAccess())
+            {
+                return dispatcher.Invoke(() => Create(title, text, notificationType, notificationCenterName, alertType));
+            }
+
             var notification = new Notification.Notification {Title = title, Text = text, NotificationType = notificationType};
             var center = GetNotificationCenter(notificationCenterName);
 
@@ -46,8 +66,7 @@ namespace Wpf.NotificationCenter.Services
         }
 
         /// <inheritdoc />
-        public Notification.Notification
-            Error(string title, string text, string? notificationCenterName = null, AlertType alertType = AlertType.All) =>
+        public Notification.Notification Error(string title, string text, string? notificationCenterName = null, AlertType alertType = AlertType.All) =>
             Create(title, text, NotificationType.Error, notificationCenterName, alertType);
 
         /// <inheritdoc />
